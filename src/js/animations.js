771b56de957690a114ababaea382a8f6f01e3f55
Devlukinnha <lucashenrique030407@gmail.com>
2025-07-26 17:39:24 -0300

    Estrutura, estilizacao e animacoes finalizadas

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,5 +1,6 @@
 ScrollReveal({reset: true});
 
+// animacao home
 ScrollReveal().reveal(".frase-desktop", {
     origin: 'top',
     distance: '50px',
@@ -43,6 +44,7 @@ ScrollReveal().reveal(".btn-contato", {
     reset: true
 });
 
+//animacao sessao sobre mim
 ScrollReveal().reveal(".foto-perfil", {
   origin: 'left',
   distance: '30px',
@@ -66,6 +68,9 @@ ScrollReveal().reveal(".paragrafo-sobre", {
   easing: 'ease-in-out',
 })
 
+//animacao da sessa skills and tools
+
+//animcao skills front-end
 ScrollReveal().reveal(".title-skills", {
   origin: 'top',
   distance: '60px',
@@ -82,18 +87,187 @@ ScrollReveal().reveal(".title-front", {
   easing: 'ease-in-out',
 });
 
-ScrollReveal().reveal(".cards-lg .card-lg", {
+ScrollReveal().reveal(".line-1", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 600,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".cards-lg-front .card-lg-front", {
   origin: 'left',
   distance: '60px',
   duration: 1000,
   interval: 500,
   easing: 'ease-in-out',
-})
+});
 
-ScrollReveal().reveal(".cards .card", {
+//animcao skills back-end
+ScrollReveal().reveal(".title-back", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 700,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".line-2", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 600,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".cards-lg-back .card-lg-back", {
   origin: 'left',
   distance: '60px',
   duration: 1000,
-  interval: 300,
+  delay: 700,
+  interval: 500,
   easing: 'ease-in-out',
-})
+});
+
+//animcao skills design 
+ScrollReveal().reveal(".title-design", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 700,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".line-3", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 600,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".cards-design .card-design", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 700,
+  interval: 500,
+  easing: 'ease-in-out',
+});
+
+//animcao projetos
+ScrollReveal().reveal(".title-projetos", {
+  origin: 'top',
+  distance: '60px',
+  duration: 1000,
+  delay: 700,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".cards .card", {
+  origin: 'left',
+  distance: '80px',
+  duration: 1000,
+  delay: 800,
+  interval: 200,
+  easing: 'ease-in-out',
+});
+
+//animcao sessao contato
+ScrollReveal().reveal(".title-contato", {
+  origin: 'top',
+  distance: '60px',
+  duration: 1000,
+  delay: 700,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".paragrafo-contato", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".label-1", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".input-1", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".label-2", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 1000,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".input-2", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".label-3", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 1000,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".input-3", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".btn-enviar", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".line-final", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  delay: 800,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".cards-btn .card-btn", {
+  origin: 'left',
+  distance: '80px',
+  duration: 1000,
+  delay: 800,
+  interval: 200,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".btn-topo", {
+  origin: 'bottom',
+  distance: '80px',
+  duration: 1000,
+  delay: 800,
+  interval: 200,
+  easing: 'ease-in-out',
+});
