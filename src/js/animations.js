8c8b19c999dab970bd3347af88ce27ea40a7bc93
Devlukinnha <lucashenrique030407@gmail.com>
2025-07-25 22:16:49 -0300

    Testando animacoes

---
+++ src/js/animations.js
@@ -0,0 +1,99 @@
+ScrollReveal({reset: true});
+
+ScrollReveal().reveal(".frase-desktop", {
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    delay: 0,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal(".title-1", {
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    delay: 800,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal(".title-2", {
+    origin: 'top',
+    distance: '50px',
+    duration: 800,
+    delay: 1200,
+    easing: 'ease-in-out'
+});
+
+ScrollReveal().reveal(".paragrafo-inicial", {
+    origin: 'left',
+    distance: '80px',
+    duration: 800,
+    delay: 1500,
+    easing: 'ease-out'
+})
+
+
+ScrollReveal().reveal(".btn-contato", {
+    opacity: 0,
+    origin: 'right',
+    distance: '50px',
+    duration: 800,
+    delay: 1800,
+    easing: 'ease-in-out',
+    reset: true
+});
+
+ScrollReveal().reveal(".foto-perfil", {
+  origin: 'left',
+  distance: '30px',
+  duration: 2000,
+  easing: 'ease-out',
+});
+
+ScrollReveal().reveal(".title-sobre", {
+  origin: 'right',
+  distance: '60px',
+  duration: 800,
+  delay: 300,
+  easing: 'ease-in-out',
+})
+
+ScrollReveal().reveal(".paragrafo-sobre", {
+  origin: 'right',
+  distance: '60px',
+  duration: 800,
+  delay: 700,
+  easing: 'ease-in-out',
+})
+
+ScrollReveal().reveal(".title-skills", {
+  origin: 'top',
+  distance: '60px',
+  duration: 800,
+  delay: 300,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".title-front", {
+  origin: 'left',
+  distance: '60px',
+  duration: 800,
+  delay: 500,
+  easing: 'ease-in-out',
+});
+
+ScrollReveal().reveal(".cards-lg .card-lg", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  interval: 500,
+  easing: 'ease-in-out',
+})
+
+ScrollReveal().reveal(".cards .card", {
+  origin: 'left',
+  distance: '60px',
+  duration: 1000,
+  interval: 300,
+  easing: 'ease-in-out',
+})
