15765bf7c2514d6a21eb52e57dae1461c021ccfa
Devlukinnha <lucashenrique030407@gmail.com>
2025-08-02 19:15:45 -0300

    Melhorias nas responsividade e troca do background

--- src/js/animations.js
+++ src/js/animations.js
@@ -31,9 +31,9 @@ revelar.reveal('.paragrafo-inicial', {
 });
 
 revelar.reveal('.btn-contato', {
-  duration: 2000,
+  duration: 1000,
   distance: '60px',
-  delay: 1500,
+  delay: 200,
   reset: true
 });
 
@@ -47,14 +47,14 @@ revelar.reveal('.foto-perfil', {
 revelar.reveal('.title-sobre', {
   origin: 'rigth',
   duration: 1000,
-  delay: 600,
+  delay: 500,
   distance: '90px',
 });
 
 revelar.reveal('.paragrafo-sobre', {
   origin: 'rigth',
   duration: 1000,
-  delay: 800,
+  delay: 700,
   distance: '90px',
 });
 
@@ -66,6 +66,7 @@ revelar.reveal('.title-skills', {
   distance: '90px',
 });
 
+//front end
 revelar.reveal('.title-front', {
   origin: 'rigth',
   duration: 800,
@@ -90,41 +91,245 @@ revelar.reveal('.card-lg-front-1', {
 revelar.reveal('.card-lg-front-2', {
   origin: 'left',
   duration: 800,
-  delay: 1500,
+  delay: 1200,
   distance: '90px',
 });
 
 revelar.reveal('.card-lg-front-3', {
   origin: 'left',
   duration: 800,
-  delay: 2000,
+  delay: 1600,
   distance: '90px',
 });
 
 revelar.reveal('.card-lg-front-4', {
   origin: 'left',
   duration: 800,
-  delay: 2500,
+  delay: 1800,
   distance: '90px',
 });
 
 revelar.reveal('.card-lg-front-5', {
   origin: 'left',
   duration: 800,
-  delay: 3000,
+  delay: 2000,
   distance: '90px',
 });
 
 revelar.reveal('.card-lg-front-6', {
   origin: 'left',
   duration: 800,
-  delay: 3500,
+  delay: 2200,
   distance: '90px',
 });
 
 revelar.reveal('.card-lg-front-7', {
   origin: 'left',
   duration: 800,
-  delay: 4000,
+  delay: 2600,
   distance: '90px',
+});
+
+
+//Design 
+revelar.reveal('.title-design', {
+  origin: 'rigth',
+  duration: 800,
+  delay: 600,
+  distance: '90px',
+});
+
+revelar.reveal('.line-2', {
+  origin: 'left',
+  duration: 800,
+  delay: 800,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-1', {
+  origin: 'left',
+  duration: 1000,
+  delay: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-2', {
+  origin: 'left',
+  duration: 800,
+  delay: 1200,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-3', {
+  origin: 'left',
+  duration: 800,
+  delay: 1600,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-4', {
+  origin: 'left',
+  duration: 800,
+  delay: 1800,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-5', {
+  origin: 'left',
+  duration: 800,
+  delay: 2000,
+  distance: '90px',
+});
+
+revelar.reveal('.card-design-6', {
+  origin: 'left',
+  duration: 800,
+  delay: 2200,
+  distance: '90px',
+});
+
+
+//Sessao projetos
+revelar.reveal('.title-projetos', {
+  origin: 'top',
+  duration: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.title-projetos-2', {
+  origin: 'top',
+  duration: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.card-projeto-1', {
+  origin: 'left',
+  duration: 1000,
+  delay: 500,
+  distance: '90px',
+});
+
+revelar.reveal('.card-projeto-2', {
+  origin: 'left',
+  duration: 1000,
+  delay: 800,
+  distance: '90px',
+});
+
+revelar.reveal('.card-projeto-3', {
+  origin: 'left',
+  duration: 1000,
+  delay: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.container-git', {
+  origin: 'bottom',
+  duration: 1000,
+  delay: 1000,
+  distance: '90px',
+});
+
+
+//Sessao Contato 
+revelar.reveal('.title-contato', {
+  origin: 'top',
+  duration: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.paragrafo-contato', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 700,
+  distance: '90px',
+});
+
+revelar.reveal('.label-1', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 800,
+  distance: '90px',
+});
+
+revelar.reveal('.input-1', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 1000,
+  distance: '90px',
+});
+
+revelar.reveal('.label-2', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 1200,
+  distance: '90px',
+});
+
+revelar.reveal('.input-2', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 1400,
+  distance: '90px',
+});
+
+revelar.reveal('.label-3', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 1600,
+  distance: '90px',
+});
+
+revelar.reveal('.input-3', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 1800,
+  distance: '90px',
+});
+
+revelar.reveal('.btn-enviar', {
+  origin: 'rigth',
+  duration: 1000,
+  delay: 2000,
+  distance: '90px',
+  reset: true
+});
+
+revelar.reveal('.line-final', {
+  origin: 'bottom',
+  duration: 1000,
+  delay: 2200,
+  distance: '60px',
+});
+
+revelar.reveal('.card-btn-1', {
+  origin: 'left',
+  duration: 1000,
+  delay: 2600,
+  distance: '90px',
+  reset: true
+});
+
+revelar.reveal('.card-btn-2', {
+  origin: 'left',
+  duration: 1000,
+  delay: 2800,
+  distance: '90px',
+  reset: true
+});
+
+revelar.reveal('.card-btn-3', {
+  origin: 'left',
+  duration: 1000,
+  delay: 3000,
+  distance: '90px',
+  reset: true
+});
+
+revelar.reveal('.btn-topo', {
+  origin: 'bottom',
+  duration: 1000,
+  delay: 3000,
+  distance: '90px',
+  reset: true
 });
